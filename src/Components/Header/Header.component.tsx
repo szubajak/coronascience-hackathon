f98cf5584274e54ddeb54336ee0e2646ed14139c
Cagri Tasci <cagri.c.tasci@outlook.de>
2020-03-28 16:31:35 +0100

    Header Text shortened and typo removed

--- src/Components/Header/Header.component.tsx
+++ src/Components/Header/Header.component.tsx
@@ -3,7 +3,7 @@ import { StyledHeaderBox, StyledLogoTypography } from './Header.styles'
 
 export const HeaderComponent: React.FC = () => (
     <StyledHeaderBox elevation={2}>
-        <StyledLogoTypography>Corona Science Web App</StyledLogoTypography>
-        <StyledLogoTypography>Powerred by Hackathon</StyledLogoTypography>
+        <StyledLogoTypography>Corona Science</StyledLogoTypography>
+        <StyledLogoTypography>Powered by Hackathon</StyledLogoTypography>
     </StyledHeaderBox>
 )
